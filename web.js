67d72495e4dc8089b21f971cc3c71955efb2dd86
McCall Bliss <missmccallkay@gmail.com>
2013-04-05 17:57:30 -0400

    write concern addressed

--- web.js
+++ web.js
@@ -3,10 +3,6 @@
 var express = require('express');
 var app = express.createServer(express.logger());
 
-var databaseUrl = "highscores"; // "username:password@example.com/mydb"
-var collections = ["users", "reports"]
-var db = require("mongojs").connect(databaseUrl, collections);
-
 // ENABLE CORS
 app.all('/', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -18,23 +14,52 @@ app.get('/', function(request, response) {
   response.send("hi");
 });
 
+/*
+var databaseUrl = "git@heroku.com:high-scores.git/highscores"; // "username:password@example.com/mydb"
+var collections = ["users", "reports"]
+var db = require("mongojs").connect(databaseUrl, collections);
+*/
+
+var mongodb = require("mongodb"),
+    mongoserver = new mongodb.Server('localhost', 2701, {safe:false,}),
+    db_connector = new mongodb.Db('highscores', mongoserver, {w:0});
+
+db_connector.open(function() {
+  // call back when the call db.open() returns
+
+  /*
+  db.collection('scores', function(err, collection) {
+    // node calls this function when the request for a collection returns
+    doc = {
+      "name" : "MongoDb",
+      "type" : "database",
+      "count" : 1,
+    }
+
+    collection.insert(doc, function() {
+      // node calls this when the insert returns
+    });
+  });
+  */
+});
+
 /* FUNCTIONS I NEED
 
 Search and find a user
+db.users.save({email: "srirangan@gmail.com", password: "iLoveMongo", sex: "male"}, function(err, saved) {
+  if( err || !saved ) console.log("User not saved");
+  else console.log("User saved");
+});
 
-db.users.find({sex: "female"}, function(err, users) {
+db.users.find({email: "srirangan@gmail.com"}, function(err, users) {
   if( err || !users) console.log("No female users found");
   else users.forEach( function(femaleUser) {
     console.log(femaleUser);
   } );
 });
 
-Save a username and score
 
-db.users.save({email: "srirangan@gmail.com", password: "iLoveMongo", sex: "male"}, function(err, saved) {
-  if( err || !saved ) console.log("User not saved");
-  else console.log("User saved");
-});
+Save a username and score
 
 */
 
