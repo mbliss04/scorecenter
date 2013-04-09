9848e469430227e944c0235037a666efc1f28875
McCall Bliss <missmccallkay@gmail.com>
2013-04-09 13:06:58 -0400

    body parser add

--- web.js
+++ web.js
@@ -2,6 +2,7 @@
 
 var express = require('express');
 var app = express.createServer(express.logger());
+app.use(express.bodyParser());
 
 app.configure(function(){
         app.set('views', __dirname + '/views');
@@ -24,45 +25,31 @@ var mongodb = require("mongodb"),
     db_connector = new mongodb.Db('highscores', mongoserver, {w:0}); 
 
 db_connector.open(function() {
-  // call back when the call db.open() returns
 
-  /*
-  db.collection('scores', function(err, collection) {
-    // node calls this function when the request for a collection returns
-    doc = {
-      "name" : "MongoDb",
-      "type" : "database",
-      "count" : 1,
-    }
-
-    collection.insert(doc, function() {
-      // node calls this when the insert returns
-    });
+  db.users.save({email: "srirangan@gmail.com", password: "iLoveMongo", sex: "male"}, function(err, saved) {
+    if( err || !saved ) console.log("User not saved");
+    else console.log("User saved");
   });
-  */
+
+  db.users.find({email: "srirangan@gmail.com"}, function(err, users) {
+    if( err || !users) console.log("No female users found");
+    else users.forEach( function(femaleUser) {
+      console.log(femaleUser);
+    } );
+  });
+
 });
 
-/* FUNCTIONS I NEED
-
-Search and find a user
-db.users.save({email: "srirangan@gmail.com", password: "iLoveMongo", sex: "male"}, function(err, saved) {
-  if( err || !saved ) console.log("User not saved");
-  else console.log("User saved");
+app.get("/submit.json", function(req, res) {
+  res.setHeader('Content-Type', 'text/json');
+  res.send('{"error: Something went wrong"}');
 });
 
-db.users.find({email: "srirangan@gmail.com"}, function(err, users) {
-  if( err || !users) console.log("No female users found");
-  else users.forEach( function(femaleUser) {
-    console.log(femaleUser);
-  } );
+app.post("/submit.json", function(req, res) {
+  res.send("done");
 });
 
-
-Save a username and score
-
-*/
-
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 7000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
